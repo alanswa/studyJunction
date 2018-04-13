48fb492fbdbbffac72fcdce35c20ab2328275f15
dasarealankar <dasarealankar@gmail.com>
2018-04-13 15:11:14 +0530

    Created Project Only

---
+++ src/studyjunction/StudyJunction.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studyjunction;
+
+/**
+ *
+ * @author admin
+ */
+public class StudyJunction {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello how are you ?");
+        System.out.println("Fine");
+    }
+    
+}
